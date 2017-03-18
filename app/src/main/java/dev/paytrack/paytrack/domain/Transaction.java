e2c7f70d42b58261a99f3a5ef50fba6349b50ce5
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-03-18 01:08:19 +0100

    add domain

---
+++ app/src/main/java/dev/paytrack/paytrack/domain/Transaction.java
@@ -0,0 +1,41 @@
+package dev.paytrack.paytrack.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Builder;
+
+/**
+ * Created by albert on 18/03/17.
+ * @author albert
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Transaction {
+
+    // The IBAN of this account
+    private String originIban;
+
+    // Amount of the transaction. If the amount is positive, the customer received money,
+    // if the amount is negative the customer lost money
+    private Number amount;
+
+    // Name of the counter party (optional)
+    private String counterPartyName;
+
+    // IBAN of the counter party (optional)
+    private String counterPartyIban;
+
+    // Payment reference
+    private String usage;
+
+    // Booking date. In the format YYYY-MM-DD
+    private String bookingDate;
+
+}
---
+++ app/src/main/java/dev/paytrack/paytrack/domain/UserInfo.java
@@ -0,0 +1,34 @@
+package dev.paytrack.paytrack.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Builder;
+
+/**
+ * Created by albert on 18/03/17.
+ * @author albert
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class UserInfo {
+
+    // First name of the user
+    private String firstName;
+
+    // Last name of the user
+    private String lastName;
+
+    // Birth date of the user. In the format YYYY-MM-DD
+    private String dateOfBirth;
+
+    // Gender of the user = ["MALE" or "FEMALE" or "UNKNOWN"]
+    private String gender;
+
+}
